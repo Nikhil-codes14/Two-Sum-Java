531688b09f94e3db05201f5b6aa044d659fda109
Nikhil Awasthi <awasthinikhil2507@gmail.com>
2026-09-09 22:00:30 +0530

    Improve Two Sum code Structure

--- Two_sum.java
+++ Two_sum.java
@@ -1,29 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Two_sum {
+    
     Scanner sc = new Scanner(System.in);
 
     void Twosum() {
         System.out.println("Enter size of Array:");
         int n = sc.nextInt();
-        System.out.println("Enter the Array elements:");
+
         int arr[] = new int[n];
+        
+        System.out.println("Enter the Array elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        
         System.out.println("Enter the Target value:");
         int Target = sc.nextInt();
 
         boolean found = false;
+        
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
+                
                 if (arr[i] + arr[j] == Target) {
-                    System.out.println(
-                            " Pair found: " + arr[i] + " + " + arr[j] + "  = " + Target);
+                    System.out.println(" Pair found: " + arr[i] + " + " + arr[j] + "  = " + Target);
                     found = true;
                 }
             }
         }
+        
         if (!found) {
             System.out.println("No correct value in given");
 
